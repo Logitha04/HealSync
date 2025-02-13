c8169bec4141a3a7a3590db08ecdc198894e37e3
Logitha04 <logithadharshini@gmail.com>
2025-02-13 19:38:51 +0530

    Initial commit

---
+++ HealSync/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealSync.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
